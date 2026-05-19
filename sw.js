9f272267a216f0fab15f3a5f3cab60b4581fad3d
simone10erre-debug <simone10erre@gmail.com>
2026-05-19 15:36:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ust-v1').then((cache) => cache.addAll(['index.html'])));
+  e.waitUntil(caches.open('ust-v2').then((cache) => cache.addAll(['index.html'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
